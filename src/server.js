ed7c066cfb2725332733f9b3b1f810eb7f3d81c9
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-03-08 20:21:09 -0300

    server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,26 @@
 import express from 'express';
+import ErrorsApp from './erros/ErrorsApp';
+import cors from 'cors';
+import router from './routes';
 
 const app = express();
+
+app.use(router);
+app.use(express.json());
+app.use(cors());
+
+app.use((error, request, response, next) => {
+  if (error instanceof ErrorsApp) {
+    return response.status(error.status).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('server iniciado na porta 3333');
