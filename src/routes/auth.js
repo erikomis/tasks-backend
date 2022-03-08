6e4e13ecff6010f2b5ca6da7080d4644cb4373cc
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-03-08 20:24:51 -0300

    rota de auth

---
+++ src/routes/auth.js
@@ -0,0 +1,8 @@
+import Auth from '../services/Auth';
+import { Router } from 'express';
+
+const routerAuth = new Router();
+routerAuth.post('/login', Auth.login);
+routerAuth.post('/register', Auth.register);
+
+export default routerAuth;
