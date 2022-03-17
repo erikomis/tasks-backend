997d41a7f08c95ecec940d0813a38df8552aa917
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-03-17 15:54:29 -0300

    routerIndex  use routerusers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import routerAuth from './auth';
+import routerUser from './user';
 
 const routerIndex = Router();
 
 routerIndex.use('/auth', routerAuth);
+routerIndex.use('/users', routerUser);
 
 export default routerIndex;
