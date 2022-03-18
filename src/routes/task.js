893723d590f609e8e9fcdcf413e6b56f8fa1e1aa
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-03-18 00:14:58 -0300

    uso do  loginRequired

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import loginRequired from '../middleware/loginRequired';
 import Tasks from '../services/Tasks';
 
 const routerTask = new Router();
 
-routerTask.post('/', Tasks.criarTask);
+routerTask.post('/', loginRequired, Tasks.criarTask);
 
 export default routerTask;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
+import loginRequired from '../middleware/loginRequired';
 import Users from '../services/Users';
 
 const routerUser = new Router();
-routerUser.get('/', Users.todosOsUsers);
-routerUser.get('/:id', Users.userPorId);
-routerUser.put('/:id', Users.atualizUser);
-routerUser.delete('/:id', Users.deleteUser);
+routerUser.get('/:id', loginRequired, Users.userPorId);
+routerUser.put('/:id', loginRequired, Users.atualizUser);
+routerUser.delete('/:id', loginRequired, Users.deleteUser);
 
 export default routerUser;
