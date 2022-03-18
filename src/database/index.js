4489627613f3fba8eed96ee3721f198538940b77
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-03-17 23:30:06 -0300

    sequelize

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
+import Task from '../models/Task';
 import User from '../models/User';
 
-const models = [User];
+const models = [User, Task];
 const connection = new Sequelize(databaseConfig);
 
 models.forEach((model) => model.init(connection));
