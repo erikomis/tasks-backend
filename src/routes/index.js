4af15f003528d0004efc4025906e78b784685da1
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-03-17 23:38:00 -0300

    uso  da rota task

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import routerAuth from './auth';
+import routerTask from './task';
 import routerUser from './user';
 
 const routerIndex = Router();
 
 routerIndex.use('/auth', routerAuth);
 routerIndex.use('/users', routerUser);
+routerIndex.use('/tasks', routerTask);
 
 export default routerIndex;
