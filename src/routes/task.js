1e5dade7e231daec116a54a420babc72fd48d0c4
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-04-13 13:48:26 -0300

    rota task

--- src/routes/task.js
+++ src/routes/task.js
@@ -5,5 +5,11 @@ import Tasks from '../services/Tasks';
 const routerTask = new Router();
 
 routerTask.post('/', loginRequired, Tasks.criarTask);
+routerTask.get('/', loginRequired, Tasks.verTodasAsTasks);
+routerTask.get('/:id', loginRequired, Tasks.taskById);
+
+routerTask.put('/:id', loginRequired, Tasks.atualizarTaskById);
+routerTask.delete('/:id', loginRequired, Tasks.deleteTaskByid);
+routerTask.post('/search', loginRequired, Tasks.buscaPorNome);
 
 export default routerTask;
