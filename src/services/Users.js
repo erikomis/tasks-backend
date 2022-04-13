8cdbbdcc5ef5e4001a7d93105887004ba6c8ae46
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-04-13 13:38:12 -0300

    users

--- src/services/Users.js
+++ src/services/Users.js
@@ -1,47 +1,31 @@
 import User from '../models/User';
 
 class Users {
-  async todosOsUsers(request, response) {
-    try {
-      const users = await User.findAll();
-
-      return response.json(users);
-    } catch (error) {
-      return response.json(error);
-    }
-  }
   async userPorId(request, response) {
     try {
-      const { id } = request.params;
-
+      const id = request.userId;
       const user = await User.findByPk(id);
-
-      return response.json(user);
+      const { nome, email } = user;
+      return response.json({ nome, email });
     } catch (error) {
       return response.json('erro');
     }
   }
 
   async atualizUser(request, response) {
-    const { id } = request.params;
-
+    const id = request.userId;
     const { nome } = request.body;
-
     const user = await User.findByPk(id);
-
     User.update(user, nome);
-
     return response.json(user);
   }
 
   async deleteUser(request, response) {
-    const { id } = request.params;
-
+    const id = request.userId;
     const user = await User.findByPk(id);
-
     await user.destroy();
 
-    return response.json('Usuario deletado  com sucesso');
+    return response.json('Usuario deletado com sucesso');
   }
 }
 
