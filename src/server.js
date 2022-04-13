515244b5b95c64747f8f6dfc8c8c3aa425e24491
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-04-13 14:18:41 -0300

    swagger

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import routerIndex from './routes';
 import { resolve } from 'path';
 import ErrorsApp from './erros/ErrorsApp';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
 dotenv.config();
 
 const app = express();
@@ -12,10 +14,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(resolve(__dirname, '..', 'uploads')));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(cors());
 app.use(routerIndex);
 
-app.use((error, request, response, next) => {
+app.use((error, request, response, _next) => {
   if (error instanceof ErrorsApp) {
     return response.status(error.status).json({
       status: 'error',
@@ -30,7 +33,5 @@ app.use((error, request, response, next) => {
 
 const port = 3333;
 app.listen(port, () => {
-  console.log();
-  console.log(`Escutando  na porta ${port}`);
-  console.log(`Escutando  na porta ${port}`);
+  console.log(`\nEscutando  na porta ${port}`);
 });
