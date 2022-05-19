650c0c5dde40a1da20a3f3c8cfb81ba9616b826b
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-05-19 00:07:15 -0300

    middleware

--- src/middleware/loginRequired.js
+++ src/middleware/loginRequired.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/User';
+import User from '../infra/sequelize/models/User';
 
 export default async (request, response, next) => {
   const { authorization } = request.headers;
